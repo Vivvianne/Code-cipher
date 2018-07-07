705926f8dde1d1df4f86bf1010450a24e4d75c7e
Vivvianne Kimani <ndiahav@gmail.com>
2018-07-07 11:00:55 +0300

    Added prompt. Workin

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 alert('Hi there.')
+
+/*
+1. Prompt.
+2. Store the answer to the promt.
+3. Store the answer to the promt inside a variable.
+4. Print it to the console.
+*/
+// We store sentence from prompt in var sentence
+var sentence = prompt()
+// We log sentence.
+console.log()
